b4ec018d355548630f117e1f6c4922cfa4069b53
Jordan Porter <jporter@ngacres.com>
2018-01-01 20:40:52 -0700

    get ready for publishing

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
         let displayRoomName = this.props.currentGameroom ? "gameroomName" : "hidden";
         return (
             <div>
-                <div className="logo" onClick={()=>{window.location.reload(true);}}>CaptionThat</div>
+                <div className="logo">CaptionThat</div>
                 <div className={displayRoomName}>{this.props.currentGameroom} - {this.props.currentGameroomUsers.length} Players</div>
             </div>
         )
